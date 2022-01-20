301816ab7a7fd9d9055a50df6f88a86949169cb8
unknown <davidb@soymomo.com>
2022-01-19 20:21:04 -0500

    feat: configutare enviroment with enzyme and create and test with it's

--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { render } from "@testing-library/react"
 import FirstApp from "../FirstApp"
-
+import {shallow} from 'enzyme'
+import '@testing-library/jest-dom'
 
 describe('testing on First App',()=>{
-    test("should of show Hi,I'm Goku", () => {
+    
+    // Don't will function because tobeInthe document is from @testing-library/jest-dom/extend-expect and not it's turn on
+    // test("should of show Hi,I'm Goku", () => {
+        
+    //     const greeting = "Hi,I'm Goku"
+    //     const {getByText}= render(<FirstApp greeting={greeting}/>)
+
+    //     expect(getByText(greeting)).toBeInTheDocument()
+    // })
+
+    test('should render <FirstApp /> correctly', () => {
         
         const greeting = "Hi,I'm Goku"
-        const {getByText}= render(<FirstApp greeting={greeting}/>)
-
-        expect(getByText(greeting)).toBeInTheDocument()
-    })
+        const wrapper = shallow(<FirstApp greeting={greeting}/>)
+        expect(wrapper).toMatchSnapshot();
+    });
+    
     
 })
